646d3b79eeb6d03a974a3f55f3d994298de352fd
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2016-08-27 07:30:09 -0700

    cleanup

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,4 +4,4 @@ cd $DIR/..
 app_name=`cat qube.json | jq .name | sed 's/\"//g'`
 instance_name=${app_name}-v1
 docker rm -f  ${instance_name}
-docker run -d --name dmz-v1 -p 443:443 -p 80:80 gcr.io/qubeship/${app_name}
+docker run -d --name ${instance_name} -p 443:443 -p 80:80 gcr.io/qubeship/${app_name}
